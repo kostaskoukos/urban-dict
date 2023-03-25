34e3bb603b24c098b2ba541c9d7485d74a2a5c86
KONSTANTINOS KOUKOS <kkonst2014@gmail.com>
2023-03-25 17:37:14 +0200

    optimized tRPC url for production

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -1,10 +1,16 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import type { Router } from "./server";
 
+const getBaseUrl = () => {
+    if (typeof window !== "undefined") return ""; // browser should use relative url
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+    return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
 export const api = createTRPCProxyClient<Router>({
     links: [
         httpBatchLink({
-            url: `http://localhost:3000/trpc`,
+            url: `${getBaseUrl()}/trpc`,
         }),
     ]
 });
