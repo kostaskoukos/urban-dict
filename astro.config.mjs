e65527428d8935938c187a81e571ad81191a5f2c
KONSTANTINOS KOUKOS <kkonst2014@gmail.com>
2023-04-17 17:24:10 +0300

    refreshed vercel adapter, bumped all deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
-// import vercel from "@astrojs/vercel/serverless";
-import vercel from "@astrojs/vercel/edge";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact()],
+  adapter: vercel()
 });
